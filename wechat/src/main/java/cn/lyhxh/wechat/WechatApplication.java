a1484e9cad9323d466e1092ee75c3f27ab9f7c75
leiyang <leiyang@edspay.com>
2018-10-16 17:50:49 +0800

    wechat项目config化

--- wechat/src/main/java/cn/lyhxh/wechat/WechatApplication.java
+++ wechat/src/main/java/cn/lyhxh/wechat/WechatApplication.java
@@ -3,12 +3,16 @@ package cn.lyhxh.wechat;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan(basePackages = "cn.lyhxh.wechat.dao")
 public class WechatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WechatApplication.class, args);
+//		SpringApplication.run(WechatApplication.class, args);
+		new SpringApplicationBuilder(WechatApplication.class).web(true).run(args);
 	}
 }
