c318838455e87223cde04e695a869d9ccfe9fbe0
leiyang <leiyang@edspay.com>
2018-10-16 18:38:48 +0800

    引入Eureka

--- config/src/main/java/com/lyhxh/config/ConfigApplication.java
+++ config/src/main/java/com/lyhxh/config/ConfigApplication.java
@@ -2,10 +2,12 @@ package com.lyhxh.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigApplication {
 
 	public static void main(String[] args) {
